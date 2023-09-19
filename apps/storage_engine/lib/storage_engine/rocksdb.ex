05910ce8135998eb854c1b8327e6de1e355ffb46
Markus Pfeiffer <markus.pfeiffer@coignear.xyz>
2023-09-19 23:35:07 +0100

    Trivial open/put/get

--- apps/storage_engine/lib/storage_engine/rocksdb.ex
+++ apps/storage_engine/lib/storage_engine/rocksdb.ex
@@ -1,10 +1,14 @@
 defmodule StorageEngine.RocksDB do
   use Rustler, otp_app: :storage_engine, crate: "storageengine_rocksdb"
 
+  defstruct [:reference]
   # When your NIF is loaded, it will override this function.
-  def add(_a, _b), do: :erlang.nif_error(:nif_not_loaded)
+  def open(_path), do: error()
+  def get_path(_db), do: error()
+  def put(_db, _key, _value), do: error()
+  def get(_db, _key), do: error()
 
-  def foobar(_a), do: :erlang.nif_error(:nif_not_loaded)
-
-  def open(), do: :erlang.nif_error(:nif_not_loaded)
+  defp error() do
+    :erlang.nif_error(:nif_not_loaded)
+  end
 end
--- apps/storage_engine/native/storageengine_rocksdb/src/lib.rs
+++ apps/storage_engine/native/storageengine_rocksdb/src/lib.rs
@@ -1,31 +1,64 @@
+use rustler::{Env, NifStruct, Term};
+
+use rustler::resource::ResourceArc;
+
+// use ArangoRocks;
+use rocksdb::DB;
+
+pub struct MyDB {
+    pub path: String,
+    pub db: DB,
+}
 
 
-use rustler::{Atom, Error, Term};
-
-use ArangoRocks;
-
-mod atoms {
-    rustler::atoms! {
-        ok,
-        error,
-    banana}
+#[derive(NifStruct)]
+#[module = "StorageEngine.RocksDB"]
+struct DBResource {
+    pub reference: ResourceArc<MyDB>,
 }
 
 #[rustler::nif]
-fn add(a: i64, b: i64) -> i64 {
-    a + b
+fn open(path : String) -> DBResource {
+   let db = DB::open_default(path.clone()).unwrap();
+
+    db.put(b"my key", b"my value").unwrap();
+   match db.get(b"my key") {
+       Ok(Some(value)) => println!("retrieved value {}", String::from_utf8(value).unwrap()),
+       Ok(None) => println!("value not found"),
+       Err(e) => println!("operational problem encountered: {}", e),
+   }
+   db.delete(b"my key").unwrap();
+
+
+    DBResource {
+        reference: ResourceArc::new(MyDB {
+            path: path.clone(),
+            db: db         })
+    }
 }
 
 #[rustler::nif]
-fn foobar(a: i64) -> i64 {
-    ArangoRocks::foo() * a
+fn get_path(res : DBResource) -> String {
+    res.reference.path.clone()
 }
 
 #[rustler::nif]
-fn open<'a>() -> Atom {
-    atoms::banana()
+fn put(res: DBResource, key: String, value: String) -> () {
+    res.reference.db.put(key, value).unwrap();
 }
 
+#[rustler::nif]
+fn get(res: DBResource, key: String) -> Result<String, String> {
+    match res.reference.db.get(key) {
+        Ok(Some(value)) => Ok(String::from_utf8(value).unwrap()),
+        Ok(None) => Err("not found".to_string()),
+        Err(e) => Err("penis".to_string())
+    }
+}
 
+fn load(env: Env, _: Term) -> bool {
+    rustler::resource!(MyDB, env);
+    true
+}
 
-rustler::init!("Elixir.StorageEngine.RocksDB", [add, foobar, open]);
+rustler::init!("Elixir.StorageEngine.RocksDB", [open, get_path, put, get], load = load);
